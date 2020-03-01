5535d5a8951b4b10d2cbba1af7afe44f1d07cbd8
kwbachman <kevin@omnitracs.com>
2020-02-29 22:51:01 -0500

    updated db connection creds

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var dbURI = 'mongodb://mongodbuser:mongodbpass@mongodb:27017/foliodb';
+var dbURI = 'mongodb://dbuser:dbuser777@mongodb:27017/foliodb';
 console.log('Connecting to MongoDB...') 
 mongoose.connect(dbURI);
 
