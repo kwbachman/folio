8315656f528c27a2774a914d6e8e7a038f4d32df
kwbachman <kevin@omnitracs.com>
2020-03-03 15:45:31 -0500

    Expanded db intitialization comments and moved them to the bottom of the db.js file

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -47,3 +47,32 @@ process.on('SIGTERM', function () {
 require('./investments');
 require('./users');
 
+// Initialize database with data
+
+// oc rsh <mongodb-pod>
+// mongo foliodb -u user -p password
+// > show collections (I think the collections are auto-created when clicking login button but they are empty)
+
+// > db.investments.save({ investor: 'kevin', password: 'kevin1', investments: [{  symbol: 'btc',  shares: 0.028644 }, {  symbol: 'iot',  shares: 100 }, {  symbol: 'eth',  shares: 0.25 }]})
+// > db.investments.find()
+
+// The users document can be initialized by calling the register api as shown at the bottom of the users.js model
+// > db.users.find()
+
+// Some other sample queries below:
+// db.inventory.insertMany( [
+//    { item: "journal", qty: 25, size: { h: 14, w: 21, uom: "cm" }, status: "A" },
+//    { item: "notebook", qty: 50, size: { h: 8.5, w: 11, uom: "in" }, status: "A" },
+//    { item: "paper", qty: 100, size: { h: 8.5, w: 11, uom: "in" }, status: "D" },
+//    { item: "planner", qty: 75, size: { h: 22.85, w: 30, uom: "cm" }, status: "D" },
+//    { item: "postcard", qty: 45, size: { h: 10, w: 15.25, uom: "cm" }, status: "A" }
+// ]);
+// 
+// var cursor = db.collection('investments').find({ 
+//   username: "kevin"
+// }).project({ password: 1 });
+// 
+// db.investments.find( { username: "kevin" }, { password: 1 } )
+// 
+// db.investments.find({ username: "kevin" }, { password: 1, _id: 0 })
+// 
--- app_api/models/investments.js
+++ app_api/models/investments.js
@@ -12,22 +12,3 @@ var investmentSchema = new mongoose.Schema({
 });
 
 mongoose.model('Investment', investmentSchema);
-
-// db.investments.save({ investor: 'kevin', investments: [{  symbol: 'btc',  shares: 0.028644 }, {  symbol: 'iot',  shares: 100 }, {  symbol: 'eth',  shares: 0.25 }]})
-
-// db.inventory.insertMany( [
-//    { item: "journal", qty: 25, size: { h: 14, w: 21, uom: "cm" }, status: "A" },
-//    { item: "notebook", qty: 50, size: { h: 8.5, w: 11, uom: "in" }, status: "A" },
-//    { item: "paper", qty: 100, size: { h: 8.5, w: 11, uom: "in" }, status: "D" },
-//    { item: "planner", qty: 75, size: { h: 22.85, w: 30, uom: "cm" }, status: "D" },
-//    { item: "postcard", qty: 45, size: { h: 10, w: 15.25, uom: "cm" }, status: "A" }
-// ]);
-// 
-// var cursor = db.collection('investments').find({ 
-//   username: "kevin"
-// }).project({ password: 1 });
-// 
-// db.investments.find( { username: "kevin" }, { password: 1 } )
-// 
-// db.investments.find({ username: "kevin" }, { password: 1, _id: 0 })
-// 
