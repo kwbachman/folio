3b6f8cbf31eb859b2a741b6c40f16e7182997cb3
kwbachman <kevin@omnitracs.com>
2020-04-27 11:55:38 -0400

    adjust db connection uri for use on ocp cluster

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
-var dbURI = 'mongodb://@mongodb:27017/foliodb';
+// don't use user/pass when running locally
+//var dbURI = 'mongodb://@mongodb:27017/foliodb';
+var dbURI = 'mongodb://dbuser:dbuser777@mongodb:27017/foliodb';
 console.log('Connecting to MongoDB...') 
 mongoose.connect(dbURI);
 
